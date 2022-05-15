61e79cbe5312c844887893c9ce8852e85b5a46dc
leehyeokju <hyeogjui4@gmail.com>
2022-05-15 21:54:09 +0900

    알고리즘

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /*https://github.com/SilverNine/spring-boot-jwt-tutorial/blob/master/src/main/java/me/silvernine/tutorial/controller/UserController.java*/
+/*https://dev-alxndr.tistory.com/37*/
 @SpringBootApplication
 public class DemoApplication {
 
